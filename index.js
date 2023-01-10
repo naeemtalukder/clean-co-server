f8225edf97899a4f0288f756109f2f8caf690d5c
naeemtalukder <naeemtalukder169135@gmail.com>
2023-01-11 01:17:03 +0600

    server create done

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.get("/", async(req, res) => {
+    res.send("Hello world");
+});
+
 app.listen(port, () => {
     console.log(`ami jamu port ${port}`);
 });
