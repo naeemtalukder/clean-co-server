985f6fb6f30c571054fc4539ae924f949be9e104
naeemtalukder <naeemtalukder169135@gmail.com>
2023-01-12 22:47:45 +0600

    mongno server connection eroor

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const express = require("express");
-const cors =require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-
 //! Warning: Do not use in production
 app.use(
-    cors({
-        origin: "*",
-    })
-    );
+  cors({
+    origin: "*",
+  })
+);
 
 app.use(express.json());
 
@@ -24,24 +23,30 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        const sevicesCollection = client.db("cleanCo").collection("service");
-        app.get("/service", async(req, res) => {
-            const services =await sevicesCollection.find({}).toArray();
+        const servicesCollection = client.db("cleanCo").collection("service");
+        app.get("/service", async (req, res) => {
+            const services = await servicesCollection.find({}).toArray();
+            console.log(services);
 
             res.send(services);
         });
       
     } finally {
-       
     }
   }
   run().catch(console.dir);
+ 
 
-
-app.get("/", async(req, res) => {
-    res.send("Hello world");
-});
-
-app.listen(port, () => {
-    console.log(`ami jamu port ${port}`);
-});
+  app.get("/user/:id", async (req, res) => {
+    const id = req.params;
+    console.log(id);
+    res.json(id);
+  });
+  
+  app.get("/", async (req, res) => {
+    res.json("Hello");
+  });
+  
+  app.listen(port, () => {
+    console.log(`Ami Dowracchi port ${port}`);
+  });
